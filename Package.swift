53fec76511422b9e571efa301854743f545275f3
9oHigh <wkdska957@naver.com>
2024-06-18 22:37:38 +0900

    Added support for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,31 @@
+//
+//  Package.swift
+//  NaverMapDemo
+//
+//  Created by Lee on 6/18/24.
+//  Copyright © 2024 NaverCorp. All rights reserved.
+//
+
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "NMapsMap",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "NMapsMap",
+            targets: ["NMapsMap"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "NMapsMap",
+            url: "https://github.com/navermaps/ios-map-sdk/releases/download/3.16.0/NMapsMap.xcframework.zip",
+            checksum: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+        )
+    ]
+)
