4651bb4b839d732888051f0756d09600ffbab221
9oHigh <wkdska957@naver.com>
2024-06-19 01:42:46 +0900

    fix: Geometry 프레임워크 제거

--- Package.swift
+++ Package.swift
@@ -18,11 +18,6 @@ let package = Package(
             name: "NMapsMap",
             url: "https://github.com/9oHigh/ios-map-sdk/releases/download/v3.18.1/NMapsMap.xcframework.zip",
             checksum: "bdcafdf8b01ac672c5727589c5ba162ccf09e75329937975e6bdd159aa79c6dc"
-        ),
-        .binaryTarget(
-            name: "NMapsGeometry",
-            url: "https://github.com/9oHigh/ios-map-sdk/releases/download/v3.18.1/NMapsGeometry.xcframework.zip",
-            checksum: "67ef40f38b9dd1678c25025afe305b3300c60386040a20368a7531058d483455"
-        ),
+        )
     ]
 )
