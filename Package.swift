d06941cf99662d6a2690ca37e534fb3e38132036
9oHigh <wkdska957@naver.com>
2024-06-19 00:23:30 +0900

    removed Pod files

--- Package.swift
+++ Package.swift
@@ -24,8 +24,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NMapsMap",
-            url: "https://github.com/navermaps/ios-map-sdk/releases/download/3.16.0/NMapsMap.xcframework.zip",
+            url: "https://github.com/navermaps/ios-map-sdk/releases/download/3.18.1/NMapsMap.xcframework.zip",
             checksum: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-        )
+        ),
+        .binaryTarget(
+            name: "NMapsGeometry",
+            url: "https://github.com/navermaps/ios-map-sdk/releases/download/3.18.1/NMapsGeometry.xcframework.zip",
+            checksum: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+        ),
     ]
 )
